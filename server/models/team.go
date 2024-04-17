2bf86c69c5e6028659561ba54f982b5da9a40898
shaykhouna <shaykhouna66@gmail.com>
2024-04-17 23:20:28 +0000

    with gameover

--- server/models/team.go
+++ server/models/team.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"log"
 	"sync"
 
 	"github.com/google/uuid"
@@ -47,8 +46,6 @@ func NewTeam(name string, size int) *Team {
 func (t *Team) StartGame() {
 	t.Lock()
 	defer t.Unlock()
-	t.Powers = t.GameMap.GeneratePowerUps()
-	log.Println("Powers", t.Powers)
 	t.Start = true
 }
 
@@ -80,7 +77,7 @@ func (T *Team) ExplodeBomb(bomb *Bomb) []string {
 	resp := new(Response)
 	resp.FromBomb(bomb.Position.X, bomb.Position.Y, bomb.Power)
 	deadPlayers = bomb.Explode(T.GameMap, T.Players, resp)
-
+	
 	resp.FromTeam(T, BombExploded)
 	// fmt.Println(resp)
 	T.Broadcast(resp)
